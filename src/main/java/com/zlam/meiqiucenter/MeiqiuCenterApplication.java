e7c96a8ad057bcde20f93f35ac992eba6bf2a1d1
aidenlam <lztaidenlam@gmail.com>
2024-04-11 11:41:45 +0800

    第一个用户中心项目初始化，第一次提交

---
+++ src/main/java/com/zlam/meiqiucenter/MeiqiuCenterApplication.java
@@ -0,0 +1,13 @@
+package com.zlam.meiqiucenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeiqiuCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeiqiuCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlam/meiqiucenter/MeiqiuCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlam.meiqiucenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeiqiuCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
